e9bf5eebb4e2f48c89a91ca94896dc59e6fef241
Anderson Viana <valladau2@gmail.com>
2021-08-03 22:57:17 -0300

    Criando componentes
    Criando módulos e componentes referente ao nova-transferencia

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,12 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
-    selector: 'app-nova-transferencia',
-    templateUrl: './nova-transferencia.component.html',
-    styleUrls: ['./nova-transferencia.component.scss']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
+export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
 
+  valor!: number;
+  destino!: number;
 
-export class NovaTransferenciaComponent{
+  transferir() {
+    console.log('Solicitada nova transferência');
+    const valorEmitir = {
+      valor: this.valor,
+      destino: this.destino,
+    };
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
 
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
 }
